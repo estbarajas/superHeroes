0fb40bf248a6e4e35ac01bbba3d63fa0f5b3add5
estbarajas <ebarajas@wolfmail.stritch.edu>
2018-07-25 08:35:30 -0500

    Edit view is working along with the actions.

--- SuperHeros/Controllers/SuperHeroController.cs
+++ SuperHeros/Controllers/SuperHeroController.cs
@@ -2,6 +2,7 @@
 using SuperHeros.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -33,6 +34,28 @@ namespace SuperHeros.Controllers
 
             }
             return View();
+        }
+        public ActionResult Details(int id = 0)
+        {
+            SuperHero superHero = db.SuperHeroe.Find(id);
+            return View(superHero);
+        }
+        public ActionResult Edit(int id)
+        {
+            SuperHero superHero = db.SuperHeroe.Find(id);
+            return View(superHero);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,AlterEgo,PrimaryAbility,SecondaryAbility,CatchPhrase")] SuperHero superHero)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(superHero).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(superHero);
         }
     }
 }
